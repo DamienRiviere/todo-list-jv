08e9174f163cfe9d3e6edd1d965be00471f9572d
DamienRiviere <riviere.damien64@gmail.com>
2020-07-13 13:46:54 +0200

    SinglePlatformListComponent created. Front-end done and recover data from API.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { VideoGamesListComponent } from './video-game/video-games-list/video-games-list.component';
 import { SingleVideoGameComponent } from './video-game/single-video-game/single-video-game.component';
 import { PlatformsListComponent } from './platform/platforms-list/platforms-list.component';
+import { SinglePlatformComponent } from './platform/single-platform/single-platform.component';
 
 
 const routes: Routes = [
   { path: 'video-games', component: VideoGamesListComponent },
   { path: 'video-games/:slug', component: SingleVideoGameComponent },
   { path: 'platforms', component: PlatformsListComponent },
+  { path: 'platforms/:id', component: SinglePlatformComponent },
   { path: '', redirectTo: 'video-games', pathMatch: 'full' }
 ];
 

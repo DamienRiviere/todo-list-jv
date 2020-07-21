cf6a74831080014f324d8dcd284aa7f037a0b64d
DamienRiviere <riviere.damien64@gmail.com>
2020-07-21 17:06:38 +0200

    Adding authentication on application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBo7WAvRf-MUcgRW-BNPvrZ6fbkF8wYG58",
+      authDomain: "todolist-jv.firebaseapp.com",
+      databaseURL: "https://todolist-jv.firebaseio.com",
+      projectId: "todolist-jv",
+      storageBucket: "todolist-jv.appspot.com",
+      messagingSenderId: "803307429760",
+      appId: "1:803307429760:web:dd7d4f6f76e6218c3eab65",
+      measurementId: "G-QHETJBEJ6N"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
---
+++ src/app/authentication/signin/signin.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  public signInForm: FormGroup;
+  public errorMessage: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+    const email = this.signInForm.get('email').value;
+    const password = this.signInForm.get('password').value;
+
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(['/video-games']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
+}
